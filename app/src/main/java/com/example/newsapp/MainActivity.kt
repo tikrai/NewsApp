c129fe419ebfbdf7391814ae707b920eef9f3c92
tikrai <tikrai@gmail.com>
2019-11-13 15:02:19 +0200

    News API response is shown in plain text

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -2,11 +2,46 @@ package com.example.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var disposable: Disposable? = null
+
+    private val newsApiServe by lazy {
+        NewsApiService.create()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_search.setOnClickListener {
+            if (edit_search.text.toString().isNotEmpty()) {
+                beginSearch(edit_search.text.toString())
+            }
+        }
+
+    }
+
+    private fun beginSearch(searchstring: String) {
+        disposable =
+            newsApiServe
+                .hitCountCheck(searchstring, "2019-10-13", "publishedAt", "57a79eac5a8f44efa2bd3408139b83f3")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    { result -> txt_search_result.text = "${result.articles[0]}" },
+                    { error -> Toast.makeText(this, error.message, Toast.LENGTH_SHORT).show() }
+                )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        disposable?.dispose()
     }
 }
---
+++ app/src/main/java/com/example/newsapp/NewsApiService.kt
@@ -0,0 +1,28 @@
+package com.example.newsapp
+
+import io.reactivex.Observable
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApiService {
+
+    @GET("everything")
+    fun hitCountCheck(
+        @Query("q") q: String,
+        @Query("from") from: String,
+        @Query("sortBy") sortBy: String,
+        @Query("apiKey") apiKey: String
+    ): Observable<Model.Result>
+
+    companion object {
+        fun create(): NewsApiService = Retrofit.Builder()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("https://newsapi.org/v2/")
+                .build()
+                .create(NewsApiService::class.java)
+    }
+}
